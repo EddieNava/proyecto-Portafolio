0f394ec646c61c38fa2dfce1b6f2ffb45224913f
Eddie Nava <eddie.nava@gmail.com>
2022-05-20 13:33:27 -0400

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+
+// console.log(navbar)
+
+// evento directamente en el html
+// <button onclick="alert('hola')" id="navbarBtn" class="navbar__btn-link">
+
+// navbarBtn.onclick = function () {
+//   alert('desde una propiedad del elemento en el html')
+// }
+
+// handler
+// const fn = function (e) {
+//   console.log(navbarNav)
+//   navbarNav.classList.toggle('active')
+// }
+
+// start navbar btn menu
+const navbarBtn = document.getElementById('navbarBtn')
+const navbarNav = document.querySelector('.navbar__nav')
+
+navbarBtn.addEventListener('click', function (e) {
+  console.log(navbarNav)
+  navbarNav.classList.toggle('active')
+})
+// end navbar btn menu
+
